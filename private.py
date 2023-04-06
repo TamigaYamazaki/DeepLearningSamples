64dc0249388fe8fc09f7a15b281c881c65a31e3b
TaigaYamazaki <taigaokame@icloud.com>
2023-04-06 16:13:51 +0900

    add API_key

--- private.py
+++ private.py
@@ -1,4 +1,6 @@
 from googleapiclient.discovery import build
 
-YOUTUBE_API_KEY = "AIzaSyB3fVYm6GJbQ6lodaKod0NhpKta1TzaBqo"
+#AIzaSyCW9tUGpTcL84Q3DuaqWmeQ2DCV4Ycycqc
+#AIzaSyB3fVYm6GJbQ6lodaKod0NhpKta1TzaBqo
+YOUTUBE_API_KEY = "AIzaSyCW9tUGpTcL84Q3DuaqWmeQ2DCV4Ycycqc"
 youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY, cache_discovery=False)
