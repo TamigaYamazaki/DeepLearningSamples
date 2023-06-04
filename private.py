64cd1f7db2000769ae43b570ddd6f61d0e08b259
TamigaYamazaki <88705780+TamigaYamazaki@users.noreply.github.com>
2023-06-04 10:06:41 +0900

    Delete private.py

--- private.py
+++
@@ -1,6 +0,0 @@
-from googleapiclient.discovery import build
-
-#AIzaSyCW9tUGpTcL84Q3DuaqWmeQ2DCV4Ycycqc
-#AIzaSyB3fVYm6GJbQ6lodaKod0NhpKta1TzaBqo
-YOUTUBE_API_KEY = "AIzaSyB3fVYm6GJbQ6lodaKod0NhpKta1TzaBqo"
-youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY, cache_discovery=False)
